b38b2f95fd9a97431a89df7b9994b236fc4e223f
Ravi Vishwakarma <ravivishwakarma825@gmail.com>
2021-03-20 22:34:55 +0530

    code optimization

--- server/routes/reviews.js
+++ server/routes/reviews.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../models/user");
+const Movie = require("../models/movie");
 const auth = require("../middlewares/auth");
 
 router.post("/", auth.authenticate, (req, res) => {
@@ -31,26 +32,71 @@ router.post("/", auth.authenticate, (req, res) => {
         res.status(400).send({ error: "User not found" });
         return;
       }
-      if (user.movies && user.movies.some(m => m.movieid === movieid)) {
+      if (user.movies && user.movies.some((m) => m.movieid === movieid)) {
         res.status(400).send({ erro: "user already rated this movie" });
         return;
       } else {
-        const movie = {
+        const usermovie = {
           movieid: movieid,
           rate: rate,
           review: review,
         };
         // console.log(req.session.userid);
-        user.movies.push(movie);
-        user.save().then(()=>{
-          
-        });
-        res.status(204).send();
-        return;
+        user.movies.push(usermovie);
+        user
+          .save()
+          .then(() => {
+            //Adding movie in movie document
+            Movie.findOne({ movieid: movieid }).then((movie) => {
+              const moviedata = {
+                email: user.email,
+                userid: req.session.userId,
+                rate: rate,
+                review: review,
+              };
+              if (movie) {
+                //if movie is found in db
+                movie.data.push(moviedata);
+                movie
+                  .save()
+                  .then(() => {
+                    res.status(204).send();
+                    return;
+                  })
+                  .catch(() => {
+                    res.status(500).send({ error: "Internal Server Error" });
+                    return;
+                  });
+              } else {
+                // if movie is not found
+                const m = new Movie({
+                  movieid: req.body.movieid,
+                });
+                m.data.push(moviedata);
+                m.save()
+                  .then(() => {
+                    res.status(204).send();
+                    return;
+                  })
+                  .catch(() => {
+                    res.status(500).send({ error: "Internal Server Error" });
+                    return;
+                  });
+              }
+            });
+          })
+          .catch(() => {
+            res.status(500).send({ error: "Internal Server Error" });
+            return;
+          });
+
+        // res.status(204).send();
+        // return;
       }
     })
     .catch(() => {
       res.status(500).send({ error: "Internal Server Error" });
+      return;
     });
 });
 
@@ -82,16 +128,40 @@ router.patch("/", auth.authenticate, (req, res) => {
       }
       // const u = user.movies.some(m => m.movieid === movieid);
       // console.log(user.movies);
-      if (user.movies  && user.movies.some(m => m.movieid === movieid)) {
+      if (user.movies && user.movies.some((m) => m.movieid === movieid)) {
         user.movies.forEach((movie) => {
           if (movie.movieid === movieid) {
             movie.rate = rate;
             movie.review = review;
           }
         });
-        user.save();
-        res.status(204).send();
-        return;
+        user.save().then(() => {
+          //updateing record in movie document
+          Movie.findOne({ movieid: movieid }).then((movie) => {
+            const moviedata = {
+              email: user.email,
+              userid: req.session.userId,
+              rate: rate,
+              review: review,
+            };
+            if (movie) {
+              //if movie is found in db
+              movie.data.push(moviedata);
+              movie
+                .save()
+                .then(() => {
+                  res.status(204).send();
+                  return;
+                })
+                .catch(() => {
+                  res.status(500).send({ error: "Internal Server Error" });
+                  return;
+                });
+            }
+          });
+        });
+        // res.status(204).send();
+        // return;
       } else {
         res.status(400).send({ error: "user did not reviewed this movie yet" });
         return;
